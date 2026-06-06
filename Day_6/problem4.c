65764db36f18e47bbc73d415d75014872ef1e34b
amishbuilds <mdamish435@gmail.com>
2026-06-06 18:48:22 +0530

    Add Day_6

---
+++ Day_6/problem4.c
@@ -0,0 +1,24 @@
+// wap a program to find X^n without pow().
+
+#include <stdio.h>
+
+int main()
+{
+    int x, n;
+    long long result = 1;
+
+    printf("Enter the base (X): ");
+    scanf("%d", &x);
+
+    printf("Enter the exponent (n): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        result *= x;
+    }
+
+    printf("%d^%d = %lld\n", x, n, result);
+
+    return 0;
+}
