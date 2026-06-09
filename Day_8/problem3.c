d30debeaaae637a50cc82fa1ab965cec2068b226
amishbuilds <mdamish435@gmail.com>
2026-06-09 20:39:34 +0530

    Add Day_8

---
+++ Day_8/problem3.c
@@ -0,0 +1,22 @@
+// wap a program to print character triangle.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
