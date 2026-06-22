c95001d127f26f8a4f21e9e1ff84dd65259ff4dd
amishbuilds <mdamish435@gmail.com>
2026-06-22 12:36:12 +0530

    Add Day_15

---
+++ Day_15/Q58.java
@@ -0,0 +1,28 @@
+package Summer_Assignment_25114CN348.Day_15;
+
+import java.util.Scanner;
+
+public class Q58 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[n - 1] = first;
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ Day_15/Q59.java
@@ -0,0 +1,28 @@
+package Summer_Assignment_25114CN348.Day_15;
+
+import java.util.Scanner;
+
+public class Q59 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int last = arr[n - 1];
+
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
